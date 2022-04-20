bec69d4fb8cb6d171d7d193a7e9d3b130a1f1a15
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-20 11:07:30 -0500

    Update box.js

--- box.js
+++ box.js
@@ -1,11 +1,14 @@
-function Box(x,y,w,h){
+function Box(x,y,w,h, letter){
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
+  this.letter = letter
   
   this.display = function(){
+   
     rect(this.x, this.y, this.w, this.h);
+     text(this.letter, this.x, this.y);
   }//end display
   
 }//end Box
