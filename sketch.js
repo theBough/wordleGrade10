848627143a1aface23d8afeb6a8ea95da1b7e874
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-20 11:06:32 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,14 @@
-let b = [];
-
-function setup() {
-  createCanvas(400, 400);
-  for(var i =0; i<5 ; i++){
-    b.push(new Box(10 + i*55,100,50,50));  
-  }
-}
-function draw() {
-  background(220);
-  for(var i =0; i<5 ; i++){
-     b[i].display();
-  }
-}
-function keyReleased(){
+function Box(x,y,w,h, letter){
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.letter = letter
   
-  console.log(key)
-}//end keyReleased
+  this.display = function(){
+   
+    rect(this.x, this.y, this.w, this.h);
+     text(this.letter, this.x, this.y);
+  }//end display
+  
+}//end Box
